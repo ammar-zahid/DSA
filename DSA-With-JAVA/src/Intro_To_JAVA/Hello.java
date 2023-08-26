919667287915bc32d5652313e00ee50dd2d2ecbc
Ammar Zahid <131453458+ammar-zahid@users.noreply.github.com>
2023-08-26 17:16:55 +0500

    Started DSA with JAVA

--- DSA-With-JAVA/src/Intro_To_JAVA/Hello.java
+++ DSA-With-JAVA/src/Intro_To_JAVA/Hello.java
@@ -1,5 +1,7 @@
+package Intro_To_JAVA;
+
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello, Started DSA");
+        System.out.print("Hello, World");
     }
 }
