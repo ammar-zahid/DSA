18e7cba484d7de82ac3695d4b3eb0531371e5272
Ammar Zahid <131453458+ammar-zahid@users.noreply.github.com>
2023-08-26 17:28:30 +0500

    Variables and Datatypes

--- DSA-With-JAVA/src/Variables_Datatypes_IO/Variables_and_Datatypes.java
+++ DSA-With-JAVA/src/Variables_Datatypes_IO/Variables_and_Datatypes.java
@@ -1,11 +1,12 @@
 package Variables_Datatypes_IO;
 
-public class Variables {
+public class Variables_and_Datatypes {
     public static void main(String[] args) {
         int age = 19;
         double percentage = 97.3;
         String name = "Ammar";
+        Boolean decision = true;
 
-        System.out.println(name + " is " + age + " years old and scored " + percentage + "% marks");
+        System.out.println(name + age + percentage + decision);
     }
 }
